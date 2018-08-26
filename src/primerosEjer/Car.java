a17d02b6dc13bc0708c461a275b30457719b1a25
Andres Da Costa <andrescostaribeiro16@gmail.com>
2018-08-27 01:54:54 +0200

    Objects practice

---
+++ src/primerosEjer/Car.java
@@ -0,0 +1,29 @@
+package primerosEjer;
+
+public class Car {
+
+	private String brand;
+	private String model;
+	private int horse;
+	
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getHorse() {
+		return horse;
+	}
+	public void setHorse(int horse) {
+		this.horse = horse;
+	}
+	
+	
+}
---
+++ src/primerosEjer/Objects.java
@@ -0,0 +1,28 @@
+package primerosEjer;
+
+public class Objects {
+
+	public static void main(String[] args) {
+		/*
+		 * with this main class we will manage the object of the class Car
+		 * */
+		
+		// initialice the objet Car
+		Car newcar = new Car();
+		
+		
+		// set the values to the object
+		newcar.setBrand("Audi");
+		newcar.setHorse(150);
+		newcar.setModel("A7");
+		
+		//get the imformation of this objet.
+		
+		System.out.println(newcar.getBrand());
+		System.out.println(newcar.getModel());
+		System.out.println(newcar.getHorse());
+		
+
+	}
+
+}
